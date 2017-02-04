d3c2ef40b7c410f6c397f46a55a040dcb300b3e0
Alexandr <buralex89@gmail.com>
2017-02-04 17:25:04 +0000

    almost round border

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function debounce(fn, delay) {
       c.contains(classFlipped) === true ? c.remove(classFlipped) : c.add(classFlipped);
     });
   }
-  /*return back*/
+  /*------------------- return back  --------------------------------------------*/
   function hoverListener(card) {
     card.addEventListener( "mouseleave", function() {
       var c = card.classList;
@@ -110,38 +110,75 @@ function myName() {
                                NAV HIDE (*classes without dot)
   
 ----------------------------------------------------------------------------- */
-function navHide(navSel, downCl, upCl) {
-  
+function navHide(navSel, downCl, upCl, area) {
+  var a = document.querySelector(area);
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
+  var navbar = document.querySelector(navSel);
+  var isOver = false;
   var navbarHeight = document.querySelector(navSel).offsetHeight;
-
+  var navCl = document.querySelector(navSel).classList;
+  
   function hasScrolled() {
-    var navCl = document.querySelector(navSel).classList;
     var st = window.pageYOffset;
     var windHeight = Math.max( document.documentElement.clientHeight, window.innerHeight );
     var documHeight = Math.max(document.body.offsetHeight, document.body.scrollHeight );
-                               
+    
+    navbar.addEventListener("mouseover", function(){ isOver = true });
+    navbar.addEventListener("mouseleave", function(){ isOver = false });
       
     // Make sure they scroll more than delta
     if(Math.abs(lastScrollTop - st) <= delta) return;
     
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-      // Scroll Down
+    if (st > lastScrollTop && st > navbarHeight && isOver == false){
+      
+      // Scroll Up
       navCl.remove(downCl);
       navCl.add(upCl);
     } else {
-        // Scroll Up
-        if(st + windHeight < documHeight) {
-          navCl.remove(upCl);
-          navCl.add(downCl);
-        }
+      // Scroll Down
+      if(st + windHeight < documHeight) {
+        navCl.remove(upCl);
+        navCl.add(downCl);
       }
-    lastScrollTop = st;
     }
+    
+     // up after some time
+    // if (navCl.contains(downCl) && st != 0 && st > navbarHeight) {
+    //   setTimeout(function(){ navCl.remove(downCl); navCl.add(upCl); }, 4000);  // up after some time
+    // }
+    
+    lastScrollTop = st;
+  }
   window.addEventListener("scroll", debounce(function() { hasScrolled() }, 250));
-}
   
+  /* ---------------------- appear when hover ---------------------- */
+
+  a.addEventListener("mouseover", mouseOver);
+  function mouseOver() {
+    navCl.remove(upCl);
+    navCl.add(downCl);
+  }
+}
+
+  /* ---------------------- remove nav when click on li ---------------------- */
+  
+function remNav(li, nav, modalBorder, removeClass, igniteClass) {
+  var li = document.querySelectorAll(li);  // array of li elements
+  for (var i = 0; i < li.length; i++) {
+      li[i].onclick = function(){
+        document.querySelector(nav).classList.toggle(removeClass);
+          
+          
+          /* ---------------------- ignite border when click on li ---------------------- */
+          document.querySelector(modalBorder).classList.add(igniteClass);
+          debounce(function() { document.querySelector(modalBorder).classList.remove(igniteClass) }, 1000)();
+      }
+  }
+}
+      
+  
+ 
